6a77bcfa767a21f0058f4cfd67f2f0a8f157103f
camilagocode <camila.cp.gomez@gmail.com>
2025-07-02 22:54:20 -0700

    Solution for Checkpoint 2 in 100 days with SwiftUI

---
+++ 100DaysofSwiftUI.playground/Pages/Checkpoint2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Create an array of strings
+var studentNames = ["Mary", "John", "John", "Sarah", "Joanne"]
+
+// Print the number of items in the array
+print(studentNames.count)
+
+// Print the number of unique items in the array
+print(Set(studentNames).count)
+
